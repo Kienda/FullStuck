f8880594e4470bffc17f0a245ea3c5e50922cd5b
AbdoulayeAbdoulaye Alhassane Diallo <alhassaneabdoulaye@gmail.com>
2025-01-18 13:20:33 -0500

    toggle done

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,9 @@
-document.querySelector('h1').innerHTML = "Good bye";
+// document.querySelector('h1').innerHTML = "Good bye";
 const lists = document.querySelector("ul");
 lists.lastElementChild.innerHTML = "Abdoulaye";
+document.getElementsByTagName("li")[2].style.color = "black";
+document.querySelector(".btn").style.backgroundColor = "red";
+document.querySelector(".btn").style.color = "white";
+// document.querySelector("button").classList.toggle("invisible");
+
+
